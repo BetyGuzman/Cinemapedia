f23ee6004e5f96e4f9db73462de2fe65a349fb0d
BetyGuzman <betyta_guzman@hotmail.com>
2024-12-21 12:00:15 -0600

    Cinepedia actualizada

--- lib/infrastructure/datasources/isar_datasource.dart
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -1,17 +1,26 @@
+import 'package:path_provider/path_provider.dart'
+    show getApplicationDocumentsDirectory;
 import 'package:cinemapedia/domain/datasource/local_storage_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:isar/isar.dart';
 
+
+
+
 class IsarDatasource extends LocalStorageDatasource {
+  
   late Future<Isar> db;
 
   IsarDatasource() {
     db = openDB();
   }
+
   Future<Isar> openDB() async {
-    if (Isar.instanceNames.isEmpty) {
-      return await Isar.open([MovieSchema], inspector: true, directory: '');
+    if ( Isar.instanceNames.isEmpty ) {
+      final dir = await getApplicationDocumentsDirectory();
+      return await Isar.open([ MovieSchema ], inspector: true,directory: dir.path);
     }
+
     return Future.value(Isar.getInstance());
   }
 
